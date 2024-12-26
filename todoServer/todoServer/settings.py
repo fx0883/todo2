c7cede49683532da5470a23cb4679ed3411ef0be
fx0883 <jackfeng8123@gmail.com>
2024-12-26 17:07:49 +0800

    11111

--- todoServer/todoServer/settings.py
+++ todoServer/todoServer/settings.py
@@ -16,8 +16,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY')
 DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = ['*']
-
+ALLOWED_HOSTS = ["*"]
 # Application definition
 INSTALLED_APPS = [
     'django.contrib.admin',
